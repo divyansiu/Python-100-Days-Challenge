76cf7730df0775124ef1b3a5c325ae693b51808e
divyansiu <quicksuccess07@gmail.com>
2026-06-09 22:56:44 +0530

    Boring Day

---
+++ Divyanshu/Day 3/Q8.py
@@ -0,0 +1,21 @@
+"""
+Question 8: Type Detective - String Multiplication
+
+Before running the code, predict the output.
+
+a = "5"
+b = 3
+
+print(a * b)
+
+After running, explain why the output occurs.
+"""
+#Solution :
+
+a = '5'
+b = 3
+print("Prediction output is 555")
+
+print(a * b)
+
+print("we got 555 because a is an strin and 3 is integer if we multiply the string with integer the string will get written integer times like here we get 555")
