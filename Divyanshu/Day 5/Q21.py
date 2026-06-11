0d44b2bd2d1a6370cf458abbd74601b71bee7541
divyansiu <quicksuccess07@gmail.com>
2026-06-11 15:07:58 +0530

    Day 5 2nd Commit

---
+++ Divyanshu/Day 5/Q21.py
@@ -0,0 +1,12 @@
+"""
+Question 21: Even or Odd Checker
+"""
+
+#Solution : 
+
+num = int(input("Enter a Number : "))
+
+if num%2==0:
+    print("Given Number is Even.")
+else:
+    print("Given number is Odd.")
---
+++ Divyanshu/Day 5/Q22.py
@@ -0,0 +1,13 @@
+"""
+Question 22: Positive Negative or Zero
+"""
+#Solution : 
+
+num = int (input("Enter a Number : "))
+
+if num == 0 :
+    print("Given Number is Zero.")
+elif num > 0 :
+    print("Given Number is Positive.")
+elif num < 0 :
+    print("Given Number is Negative.")
